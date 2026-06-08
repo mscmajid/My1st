b7cf3d26734263379eec034184d8ab036cf8c547
mscmajid <mscmajid@gmail.com>
2026-06-08 16:33:06 +0330

    For loop test

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
 int main(){
+    for (int i=0;i<10;i++)
+        cout<<"Hello World!\n";
+    /*cout<<"Hello World!\n";
     cout<<"Hello World!\n";
     cout<<"Hello World!\n";
     cout<<"Hello World!\n";
     cout<<"Hello World!\n";
-    cout<<"Hello World!\n";
-    cout<<"Hello World!\n";
-    cout<<"Hello World!\n";
+    cout<<"Hello World!\n";//*/
     
     int n;
     cin>>n;
