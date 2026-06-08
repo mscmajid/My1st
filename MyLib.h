f8810d00ca6f59b04e96c6d47c2e923db9910069
mscmajid <mscmajid@gmail.com>
2026-06-08 16:42:15 +0330

    Adding MyLib.h

---
+++ MyLib.h
@@ -0,0 +1,17 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+#include <iostream>
+using namespace std;
+
+void printHello(){
+    for (int i=0;i<10;i++)
+        cout<<"Hello World!\n";
+    /*cout<<"Hello World!\n";
+    cout<<"Hello World!\n";
+    cout<<"Hello World!\n";
+    cout<<"Hello World!\n";
+    cout<<"Hello World!\n";
+    cout<<"Hello World!\n";//*/
+}
+
+#endif
